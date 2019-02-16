5c54fe7d1aa292c9148ef879c4fb0aa6f90f224a
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2019-02-16 20:28:58 +0530

    in Money management module added Navigation Pane

--- app/src/main/java/com/example/myapplication1/MainActivity5.java
+++ app/src/main/java/com/example/myapplication1/MainActivity5.java
@@ -1,13 +1,36 @@
 package com.example.myapplication1;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity5 extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        setUpToolbar();
+    }
+
+    private void setUpToolbar() {
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);  // as we have removed  Title bar , so this command tells android to consider our toolbar as action bar
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+
     }
 }
