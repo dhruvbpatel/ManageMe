085cc4f9229f32a5c7cc3e7ab17c992e57c1252d
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2019-02-17 00:07:14 +0530

    Added: close the drawer pane as soon as it is clicked

--- app/src/main/java/com/example/myapplication1/MainActivity5.java
+++ app/src/main/java/com/example/myapplication1/MainActivity5.java
@@ -1,5 +1,6 @@
 package com.example.myapplication1;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -7,6 +8,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -23,28 +25,34 @@ public class MainActivity5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+
         setUpToolbar();
         navigationView = (NavigationView)findViewById(R.id.navigation_menu);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.nav_home:
                         Toast.makeText(MainActivity5.this, "Clicked Home ", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(Gravity.START);   // for closing drawer as soon as we click ot
                         break;
                     case R.id.nav_account:
                         Toast.makeText(MainActivity5.this, "Clicked Account", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(Gravity.START);   // for closing drawer as soon as we click ot
                         break;
                     case R.id.nav_todo:
                         Toast.makeText(MainActivity5.this, "Clicked Todo", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(Gravity.START);   // for closing drawer as soon as we click ot
                         break;
                     case R.id.nav_settings:
                         Toast.makeText(MainActivity5.this, "Clicked Settings", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(Gravity.START);   // for closing drawer as soon as we click ot
                         break;
                     case R.id.nav_about:
-                        Toast.makeText(MainActivity5.this, "Sab ke Sab Chutiye Hai", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity5.this, "Sab Gandu Log hai !!", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(Gravity.START);   // for closing drawer as soon as we click ot
                         break;
-
 
 
                 }
